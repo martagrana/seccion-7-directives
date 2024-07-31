5d47f5c286aa67d081ef36baf3e2b614809be5fb
Marta Grana <marta.grana.fdez@gmail.com>
2024-07-31 17:28:50 +0200

    156. building another directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { NgIf } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
+import { AuthDirective } from './auth/auth.directive';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { AuthService } from './auth/auth.service';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
 })
 export class AppComponent {
 
